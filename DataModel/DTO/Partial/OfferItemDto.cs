0c2bee0903e939dd0e5a6bd3e261d3bd81bba059
TooniceBadleg <predrag.zlonoga@ka.t-com.hr>
2017-04-03 19:02:17 +0200

    DTO objekti za EF datamodel.

---
+++ DataModel/DTO/Partial/OfferItemDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public partial class OfferItemDto
+    {
+        public string FishName { get; set; }
+
+
+    }
+}
---
+++ DataModel/DTO/Partial/UsrDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DTO
+{
+    public partial class UsrDto
+    {
+
+
+
+        //dodatak
+        public int? IdShip { get; set; }
+        //dodatak
+        //public int? IdCompany { get; set; }
+        public CompanyDto Company { get; set; }
+
+
+
+    }
+}
